01e770e98231671d48a8ceb08ed2acab6f7f6a82
d-romualdi <doriana.romualdi@mail.utoronto.ca>
2024-03-12 17:33:04 -0400

    trying to use a different funtion to rename fire column names

--- organising_fire_mpb_df.R
+++ organising_fire_mpb_df.R
@@ -30,10 +30,22 @@ fire <- read_sf("~/RBR_bias_project/Data/Albers_bc_nfdb/NFDB_poly_20210707_BC_al
 colnames(fire)
 
 # Need Fire_ID, Fire_Year, and geometry columns only
-colnames(fire)[colnames(fire) == 'YEAR'] <- 'Fire_Year'
-colnames(fire)[colnames(fire) == 'FIRE_ID'] <- 'Fire_ID'
+# Rename columns using dplyr
 
-fire2 <- fire[, c(2, 4, 29)]
+# Assuming 'sf_object' is your sf object and 'old_column_name' and 'new_column_name' are the old and new column names respectively
+fire_new <- fire %>%
+  rename(Fire_Year = YEAR)
+
+fire_new <- fire %>%
+  rename(Fire_Year = YEAR,
+         Fire_ID = FIRE_ID)
+
+colnames(fire_new)
+
+# colnames(fire)[colnames(fire) == 'YEAR'] <- 'Fire_Year'
+# colnames(fire)[colnames(fire) == 'FIRE_ID'] <- 'Fire_ID'
+
+fire2 <- fire_new[, c(2, 4, 29)]
 colnames(fire2)
 
 # Write new fire df with select columns only
@@ -52,8 +64,14 @@ head(fire_polys)
 # Checking 
 identical(fire2$Fire_ID, fire_2D$Fire_ID)
 
+# str
+str(fire_polys)
+
 
 ### Unionized MPB df ###
 mpb_union_shp <- read_sf("C:/Users/doria/Documents/RBR_bias_project/Data/mpb_single_row_per_year.shp")
 colnames(mpb_union_shp)
+
+# str
+str(mpb_union_shp)
 
